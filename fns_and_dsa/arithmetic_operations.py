83006b7db44cbcfa95a128bd48a78e1a961b229a
Brian Ochessi <ochessibrian@gmail.com>
2025-06-06 14:41:17 +0300

    Added arithmetic_operations.py script

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+           return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2
+    else:
+        return f"Unknown operation: {operation}"
+
+# Example usage:
+#print(perform_operation(10, 5, 'add'))
+#print(perform_operation(50, 20, 'subtract'))
+#print(perform_operation(6, 7, 'multiply'))
+#print(perform_operation(8, 2, 'divide'))
+#print(perform_operation(10, 0, 'divide'))   
